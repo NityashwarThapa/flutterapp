e03b6f522e6cbccf51cb2dcfeb81adbb2a88e047
NityashwarThapa <136233600+NityashwarThapa@users.noreply.github.com>
2024-12-12 10:17:55 +0545

    list and grid view

--- batch33/lib/list_and_grid_view.dart
+++ batch33/lib/list_and_grid_view.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-class DataTableScreen extends StatelessWidget {
-  const DataTableScreen({super.key});
+class DataTableView extends StatelessWidget {
+  const DataTableView({super.key});
 
   Widget displayDataTable() {
-    return DataTable(
-      border: TableBorder.all(color: Colors.black),
-      columns: const [
-        DataColumn(label: Text('Name')),
-        DataColumn(label: Text('Roll No')),
-      ],
-      rows: const [
-        DataRow(cells: [
-          DataCell(Text('Nityashwar Thapa')),
-          DataCell(Text('220423')),
-        ]),
-        DataRow(cells: [DataCell(Text("Ram")), DataCell(Text("111111"))])
-      ],
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: DataTable(
+        headingRowColor: WidgetStateColor.resolveWith(
+          (states) => Colors.lightGreenAccent,
+        ),
+        border: TableBorder.all(color: Colors.black),
+        columns: const [
+          DataColumn(
+            label: Expanded(
+              child: Text(
+                'Name',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          DataColumn(
+            label: Expanded(
+              child: Text(
+                'Roll No',
+              ),
+            ),
+          )
+        ],
+        rows: const [
+          DataRow(cells: [
+            DataCell(Text('Ram')),
+            DataCell(Text('111111')),
+          ]),
+          DataRow(
+              cells: [DataCell(Text("Nityashwar")), DataCell(Text("220423"))])
+        ],
+      ),
     );
   }
 
